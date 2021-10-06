cffc4fbf7a43a5273ba588f1ccae2c428804093d
David Annebicque <david.annebicque@gmail.com>
2021-10-06 15:02:20 +0200

    Twig et couleurs

--- src/Controller/ColorController.php
+++ src/Controller/ColorController.php
@@ -3,16 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ColorController extends AbstractController
 {
+
+
     #[Route('/color/{couleur}', name: 'color')]
     public function index($couleur): Response
     {
         return $this->render('color/index.html.twig', [
-            'couleur' => $couleur
+            'couleur' => $couleur,
+            'couleurs' => ['blue', 'red', 'yellow', 'orange', 'green', 'pink']
         ]);
+    }
+
+    #[Route('/request', name: 'request')]
+    public function request(Request $request): Response
+    {
+        dump($request);
     }
 }
